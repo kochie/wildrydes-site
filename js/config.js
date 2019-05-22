a678958f8b832ee0dfb0bd7f628859a8487d203c
Robert Koch <robert@kochie.io>
2019-05-22 14:49:35 +0800

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-1_JgDTiTk4O', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7865ce5b40t6h6dt404aaqqlh0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4d0qg9a3nv6829niv6fdlac15', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
